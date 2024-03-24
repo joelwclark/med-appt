e277b5698d935131c90aa352c6e8d371a16654ed
joelwclark <155492402+joelwclark@users.noreply.github.com>
2024-03-24 16:54:18 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const API_URL = window.location.hostname === "https://joeyclark98-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" : "add your theia server side url";
+export const API_URL = window.location.hostname === "localhost" ? "https://joeyclark98-8081.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai" : "https://joeyclark98-8081.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai";
 console.log(
     "API_URL :",
     API_URL
